1a2753703b2e0f37ec2f7c980d9dabac6dc59852
keerthanaamal <keerthanagopi112@gmail.com>
2025-12-05 15:27:34 +0530

    second commit

--- urls.py
+++ urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+print("cghdgccccccccccccc")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
